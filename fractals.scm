74c07925f5ba0b0db7a01f665c162913f660e33f
Stefan Kangas <stefankangas@gmail.com>
2017-10-30 17:55:43 +0100

    Add copyright notice

--- fractals.scm
+++ fractals.scm
@@ -10,6 +10,20 @@
             (list x iterate))
        (checker (+ (* x x) c) c (- iterate 1))))
   (checker 0 c 200))
+;; Copyright (C) 2017 Stefan Kangas
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (define r-min -1.5)
 (define r-max 0.5)
