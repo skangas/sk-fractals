c598c5b3da981a7a81e841bd2a932724d5515c54
Stefan Kangas <stefankangas@gmail.com>
2022-09-22 17:17:30 +0200

    Update copyright year

--- sk-fractals.rkt
+++ sk-fractals.rkt
@@ -1,7 +1,7 @@
 #!/usr/bin/racket
 #lang racket
 
-;; Copyright (C) 2017 Stefan Kangas
+;; Copyright (C) 2017-2022 Stefan Kangas
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
